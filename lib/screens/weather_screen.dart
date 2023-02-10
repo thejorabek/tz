d6e50393b62ac43ab7fe8de68e52a97203029cf1
thejorabek <boyxorozovjorabek@gmail.com>
2023-02-10 12:09:58 +0500

    Ui changes

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -28,24 +28,57 @@ class _WeatherScreenState extends State<WeatherScreen> {
             fit: BoxFit.cover,
           ),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: width*.12,vertical: height*.12),
+            margin: EdgeInsets.symmetric(
+                horizontal: width * .12, vertical: height * .12),
             width: double.infinity,
             height: height * .6,
             decoration: BoxDecoration(
-                border: Border.all(width: width*.007,color: Colors.blue),
+                border: Border.all(width: width * .007, color: Colors.blue),
                 borderRadius: BorderRadius.circular(15)),
           ),
           Padding(
-            padding: EdgeInsets.only(left: width*.36,top: height*.115),
-            child: Container(width: width*.29,height: height*.01,color: Colors.white)
-          ),
+              padding: EdgeInsets.only(left: width * .36, top: height * .115),
+              child: Container(
+                  width: width * .29,
+                  height: height * .01,
+                  color: Colors.white)),
           Padding(
-            padding: EdgeInsets.only(left: width*.37),
-            child: Image.asset('assets/gerb.png',width: width*.27,height: height*.27,)
-          ),
+              padding: EdgeInsets.only(left: width * .37),
+              child: Image.asset(
+                'assets/gerb.png',
+                width: width * .27,
+                height: height * .27,
+              )),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(children: [Text('11:27',style: TextStyle(color: Colors.blue,fontSize: 90,fontWeight: FontWeight.bold),)],),
+            padding: EdgeInsets.symmetric(
+                horizontal: width * .2, vertical: height * .27),
+            child: Column(
+              children: [
+                Text(
+                  '11:27',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 90,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: height * .02),
+                Text(
+                  '12 май',
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 55,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: height * .03),
+                Text(
+                  'Сешанба',
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 50,
+                      ),
+                )
+              ],
+            ),
           )
         ]),
       ),
